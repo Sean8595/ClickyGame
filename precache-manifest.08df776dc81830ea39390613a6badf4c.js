10e9998df4913d5c14387f47c3628d5ddee106c2
Sean Melvin <seanmelvin8595@gmail.com>
2019-12-20 14:34:39 -0500

    Updates

---
+++ precache-manifest.08df776dc81830ea39390613a6badf4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc0c5462c65645641048d00808576e4",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "8cd5e8d70ec416099a86",
+    "url": "/ClickyGame/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "64a93ac31c8825e478e3",
+    "url": "/ClickyGame/static/css/main.127cd51e.chunk.css"
+  },
+  {
+    "revision": "8cd5e8d70ec416099a86",
+    "url": "/ClickyGame/static/js/2.4d211e2b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ClickyGame/static/js/2.4d211e2b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64a93ac31c8825e478e3",
+    "url": "/ClickyGame/static/js/main.7a08e3d4.chunk.js"
+  },
+  {
+    "revision": "8f46facbf3b49e2da4ef",
+    "url": "/ClickyGame/static/js/runtime-main.c1792ce6.js"
+  }
+]);
